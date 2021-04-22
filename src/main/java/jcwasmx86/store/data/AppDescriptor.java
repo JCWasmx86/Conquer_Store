4eecde58413490063a0b74a58285f782f13899fb
JCWasmx86 <JCWasmx86@t-online.de>
2021-04-22 19:35:55 +0200

    Added shading

--- src/main/java/jcwasmx86/store/data/AppDescriptor.java
+++ src/main/java/jcwasmx86/store/data/AppDescriptor.java
@@ -2,6 +2,19 @@ package jcwasmx86.store.data;
 
 import java.net.URL;
 
-public record AppDescriptor(URL downloadBundle, String name, String description, String logo, String[] imageURLs) {
+/**
+ * An descriptor representing an "app". An app is either a plugin, a strategy, just a library and so on.
+ * {@code downloadBundle} is the direct link to the .zip file. {@code uniqueIdentifier} is an globally unique identifier
+ * used for dependencies. {@code installedSize} is the estimated size in bytes after installation. {@code name} is
+ * the user-facing
+ * name of the app. {@code description} is the description shown in the store. {@code logo} is the thumbnail in the
+ * minified view.
+ * {@code imageURLs} are URLs as showcases. {@code dependencies} are the dependencies of the app. These are the
+ * {@code uniqueIdentifier} of an app.
+ * {@code isLibrary} signals, that this app shouldn't be listed.
+ */
+public record AppDescriptor(URL downloadBundle, String uniqueIdentifier, long installedSize, String name,
+							String description, URL logo,
+							URL[] imageURLs, String[] dependencies, boolean isLibrary) {
 
 }
