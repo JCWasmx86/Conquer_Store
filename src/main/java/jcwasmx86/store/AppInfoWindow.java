101557192cc0e2f41d63f7d18a014d8851592bd7
JCWasmx86 <JCWasmx86@t-online.de>
2021-04-23 14:34:10 +0200

    Formatting

--- src/main/java/jcwasmx86/store/AppInfoWindow.java
+++ src/main/java/jcwasmx86/store/AppInfoWindow.java
@@ -1,8 +1,6 @@
 package jcwasmx86.store;
 
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
